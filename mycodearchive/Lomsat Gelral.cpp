e4ec2230784113cd88e07ceb235c0296ff94f380
hiensumi <phamhiennhien@gmail.com>
2023-12-20 21:30:41 +0700

    ΓÅüΓèæΓƒÆ ΓïöΓì£Γì£ΓïÅ ΓìÖΓÅâΓîç ΓîçΓì£ ΓÅÜΓƒÆΓÅâΓÄìΓÅüΓƒƒΓÄÄΓÄìΓî░ ΓÅüΓèæΓÅâΓÅü ΓÅüΓèæΓƒÆΓìÇΓƒÆ ΓìÖΓÅâΓîç ΓÅâ ΓïöΓƒƒΓìÇΓìÇΓì£ΓìÇ ΓƒƒΓïÅ ΓÅüΓèæΓƒÆ Γì£ΓÿèΓƒÆΓÅâΓïÅ

--- mycodearchive/Lomsat Gelral.cpp
+++ mycodearchive/Lomsat Gelral.cpp
@@ -1,79 +1,134 @@
-#include <bits/stdc++.h>
+// hiensumi: Maybe, success will come tomorrow. Thus, just keep trying! =) "Z/x
+#include "bits/stdc++.h"
+#define fod(i,a,b) for(int i = a;i <= b; i++)
+#define fok(i,a,b) for(int i = a;i >= b; i--)
+#define ll long long
+#define int long long
+#define fi first
+#define se second
+#define mask(i) (1LL<<(i))
+#define BITpos(a,i) ((a >> i) & 1LL)
+#define pb push_back
+#define el '\n'
+#define all(v) v.begin(), v.end()
+#define odd(i) (i & 1LL)
 using namespace std;
-
-const int N = 1e5 + 15;
-int n, c[N], sub[N], st[N], ft[N], dfa[N], cnt[N], num[N];
-vector<int> adj[N];
-long long sum[N];
-
-void dfs(int v, int p) {
-	static int time = 0;
-	dfa[st[v] = time++] = v;
-	for (int u: adj[v])
-		if (u != p) {
-			dfs(u, v);
-			sub[v] += sub[u];
+typedef pair<int, int> ii;
+const int MOD = 1e9 + 7;
+inline void kill(){cerr << "\nTime: " << clock() << "ms\n"; cerr << "⏁⊑⟒ ⋔⍜⍜⋏ ⍙⏃⌇ ⌇⍜ ⏚⟒⏃⎍⏁⟟⎎⎍⌰ ⏁⊑⏃⏁ ⏁⊑⟒⍀⟒ ⍙⏃⌇ ⏃ ⋔⟟⍀⍀⍜⍀ ⟟⋏ ⏁⊑⟒ ⍜☊⟒⏃⋏.\n"; exit(0);}
+inline void add(int &x, int y, int mod = MOD) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod;}
+inline void mul(int &x, int y, int mod = MOD) { x = (x * 1LL * y) % mod;}
+inline int bpow(int x, int y, int mod = MOD) { int ans = 1; while (y) { if (y & 1) mul(ans, x, mod); mul(x, x, mod); y >>= 1;} return ans;}
+inline int bp(int a, int b){int res = 1; while (b > 0) {if (b & 1) res = res * a; a = a * a; b >>= 1; } return res;}
+inline int Inv(int x, int mod = MOD) { return bpow(x, mod - 2, mod);}
+inline int Div(int x, int y, int mod = MOD) { int tmp = x; mul(tmp, Inv(y, mod), mod); return tmp;}
+template<class T> bool mini(T& a,T b){return (a>=b)?a=b,1:0;}
+template<class T> bool maxi(T& a,T b){return (a<=b)?a=b,1:0;}
+struct point{int x, y;};
+struct edge{int u, v, c;};
+//int find(int u){if (lab[u] < 0) return u; return lab[u] = find(lab[u]);}
+//bool join(int u, int v){u = find(u);v = find(v);if(u == v) return 0;if(lab[u] > lab[v]) swap(u,v);lab[u] += lab[v];lab[v] = u; return 1;}
+const ll INF = 1e18, base = 1e6 + 5, multitest = 0;
+//"Life is a daring adventure or it is nothing at all." -Helen Keller...
+//"Success isn't determined by how many times you win, but by how you play the week after you lose." -Pele...
+#define name ""
+#define ld long double
+// remember to reset value for multitestcase
+// she is your motivation!!!
+int n, c[base], res[base], mx[base];
+vector <int> adj[base];
+// sack (DSU on Tree)
+void init(){
+ 	   
+}
+#define um unordered_map<int,int>
+void dfs(int u, int p, um & Large){
+	res[u] = c[u];
+	Large[c[u]]++;
+	mx[u] = 1;
+	for(int v : adj[u]) if(v != p){
+		um Small;
+		dfs(v,u,Small);
+		
+		if(Small.size() > Large.size()){
+			swap(Small, Large);
+			res[u] = res[v];
+			mx[u] = mx[v];
 		}
-	sub[v]++;
-	ft[v] = time;
-}
-
-void update(int v, int u) {
-	cnt[c[u]]++;
-	if (cnt[c[u]] == num[v])
-		sum[v] += c[u];
-	else if (cnt[c[u]] > num[v])
-		sum[v] = c[u], num[v] = cnt[c[u]];
-}
-
-void sack(int v, int p, bool k) {
-	int mx = -1, vl;
-	for (int u: adj[v])
-		if (u != p && sub[u] > mx)
-			mx = sub[u], vl = u;
-
-	for (int u: adj[v])
-		if (u != p && u != vl)
-			sack(u, v, false);
-
-	if (mx > 0) {
-		sack(vl, v, true);
-		sum[v] = sum[vl], num[v] = num[vl];
-		for (int i = st[v]; i < st[vl]; i++)
-			update(v, dfa[i]);
-		for (int i = ft[vl]; i < ft[v]; i++)
-			update(v, dfa[i]);
+		
+		for(ii x : Small){
+			Large[x.fi] += x.se;
+			if(Large[x.fi] > mx[u]) mx[u] = Large[x.fi], res[u] = x.fi;
+			else if(Large[x.fi] == mx[u]) res[u] += x.fi;
+		}
 	}
-	else 
-		update(v, v);
-
-	if (!k)
-		for (int i = st[v]; i < ft[v]; i++)
-			cnt[c[dfa[i]]]--;
+	
 }
-
-void read_input() {
+void inp(){
 	cin >> n;
-	for (int i = 1; i <= n; i++)
-		cin >> c[i];
-	for (int i = 0; i < n - 1; i++) {
-		int v, u;
-		cin >> v >> u;
-		adj[v].push_back(u);
-		adj[u].push_back(v);
+	fod(i,1,n) cin >> c[i];
+	fod(i,1,n-1){
+		int u, v; cin >> u >> v;
+		adj[u].pb(v);
+		adj[v].pb(u);
 	}
 }
 
-void solve() {
-	dfs(1, 0);
-	sack(1, 0, false);
-
-	for (int i = 1; i <= n; i++)
-		cout << sum[i] << ' ';
+namespace sub_task1{
+    void solve(){
+    	um Big;
+    	dfs(1,0,Big);
+    	fod(i,1,n){
+    		cout << res[i] << " ";
+    	}
+    }	
+    
 }
 
-int main() {
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	read_input(), solve();
-	return cout << '\n', 0;
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    if(fopen(name".inp", "r")){
+      freopen(name".inp", "r", stdin);
+      freopen(name".out", "w", stdout);
+    }
+    int Test = 1; if(multitest) cin >> Test;
+    init();
+    while(Test--){
+        inp();
+        sub_task1 :: solve();
+    }
+    kill();
 }
+/*
+										Trú mưa nơi gốc cây ngày xưa
+										Để nhìn em lần cuối trong mưa
+										Để nắm tay đưa em đi về
+										Chốn mộng mơ...
+										
+										Có hôm mây gió chợt ca vang
+										Nụ cười em như nắng mùa thu sang
+										Làm lòng tôi xao xuyến mà lang thang
+										Nghĩ về em...
+										
+										Mình tôi thao thức
+										mình tôi day dứt
+										Cớ sao em không về với tôi 
+										Mình tôi thao thức
+										mình tôi day dứt
+										Cớ sao em không cười với tôi
+										Gió mang câu ca về nơi đây
+										gió mang câu ca về với đời em
+										Nắng mang câu thơ về nơi đây
+										chính em mang thơ về với tình ta ...
+										
+										Chiếc radio của em
+										Và từng ly trà đá ly kem
+										Cùng hát lên câu ca êm đềm
+										giữa mùa yêu
+										
+										Những bông hoa xanh ngoài hiên
+										Vào buổi chiều tràn nắng an nhiên
+										Ta ngồi bên cạnh nhau 
+										ngắm mùa thu sang...
+#listening to mai_khanh's music while coding
+*/
