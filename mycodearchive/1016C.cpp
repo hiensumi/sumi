6d8542aabe6ab5732e766048ce51c4cc60907c31
hiensumi <phamhiennhien@gmail.com>
2024-01-22 22:51:39 +0700

    Create 1016C.cpp

---
+++ mycodearchive/1016C.cpp
@@ -0,0 +1,122 @@
+// hiensumi: Maybe, success will come tomorrow. Thus, just keep trying! =) "Z/x
+#include "bits/stdc++.h"
+using namespace std; 
+#define            int  long long
+#define             ll  long long 
+#define             db  double 
+#define             ve  vector 
+#define             vi  vector<int>
+#define            vll  vector<ll>
+#define            str  string
+#define             pb  push_back
+#define             pk  pop_back
+#define             el  '\n'
+#define            pii  pair<int,int>
+#define            pll  pair<ll,ll>
+#define             mp  make_pair 
+#define             fi  first 
+#define             se  second
+#define         uni(a)  sort(all(a)),a.resize(unique(all(a))-a.begin()) 
+#define     fod(i,a,b)  for(int i=(int)(a);i<=(int)(b);i++)
+#define    fok(i,a,b)  for(int i=(int)(a);i>=(int)(b);i--)
+#define         all(a)  a.begin(),a.end()  
+#define             LB  lower_bound
+#define             UB  upper_bound 
+#define            tct  template<class T>
+#define     BIT(msk,i)  (msk>>(i)&1)
+#define        mask(i)  (1ll<<(i))
+#define          SZ(_)  (int)(_.size())
+#define           btpc  __builtin_popcountll
+#define            ctz  __builtin_ctzll 
+ll lg(ll a){return __lg(a);}
+ll sq(ll a){return a*a;}  
+ll gcd(ll a,ll b){return __gcd(a,b);} 
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll rd(ll l , ll r ){return l+1LL*rand()*rand()%(r-l+1);}
+#define pra(a,n) fod(_i,1,n)cout<<a[_i]<<el;cout<<el;
+#define prv(a) for(auto _v:a)cout<<_v<<el; cout<<el; 
+#define DEBUG(x) cout << #x << " = " << x << el;
+struct point{int x, y;};
+struct edge{int u, v, c;};
+const int MOD = 1e9 + 7;
+inline void kill(){cerr << "\nTime: " << clock() << "ms\n"; cerr << " [ .... EtnallirB AnuL ...].\n"; exit(0);}
+inline int bpow(int x, int y, int mod = MOD) { int ans = 1; while (y) { if (y & 1) ans = (ans % mod * x % mod + mod) % mod; x = (x % mod * x % mod + mod) % mod; y >>= 1;} return ans;}
+inline int bp(int a, int b){int res = 1; while (b > 0) {if (b & 1) res = res * a; a = a * a; b >>= 1; } return res;}
+inline int Inv(int x, int mod = MOD) { return bpow(x, mod - 2, mod);}
+template<class T> bool mini(T& a,T b){return (a>b)?a=b,1:0;}
+template<class T> bool maxi(T& a,T b){return (a<b)?a=b,1:0;}
+#define name ""
+#define ld long double
+//"Life is a daring adventure or it is nothing at all." -Helen Keller...
+//"Success isn't determined by how many times you win, but by how you play the week after you lose." -Pele...
+const ll base = 1e6 + 5, INF = 1e18, multitest = 0, endless = 0; 
+const ld PI = acos(-1) , EPS = 1e-9;
+void init(){} // remember to reset value for multitestcase
+int n, a[base], b[base];
+void inp(){
+	cin >> n;
+	fod(i,1,n) cin >> a[i];
+	fod(i,1,n) cin >> b[i];
+}
+
+namespace sub1{
+    int suf[base], fup[base], fown[base], zig[base];
+    void solve(){
+    	fok(i,n,1) suf[i] = suf[i+1] + a[i] + b[i];
+    	// still 1 index
+    	fup[n - 1] = a[n-1] + a[n] * 2 + b[n] * 3 + b[n-1] * 4;
+    	fown[n - 1] = b[n-1] + b[n] * 2 + a[n] * 3 + a[n-1] * 4;
+    	fup[n] = a[n] + 2 * b[n];
+    	fown[n] = b[n] + 2 * a[n];
+    	
+    	fok(i,n-2,1){
+    		fup[i] = a[i] + fup[i+1] + suf[i+1] + b[i] * (2 * (n - i + 1));
+    		fown[i] = b[i] + fown[i+1] + suf[i+1] + a[i] * (2 * (n - i + 1));
+    	}
+    	
+    	zig[1] = 0 * a[1] + 1 * b[1];
+    	int cnt = 1;
+    	// fixing 1 index to 0 index
+    	fod(i,2,n) zig[i] = zig[i-1] + ((i + 1) & 1 ? 
+    		b[i] * (cnt + 1) + a[i] * (cnt + 2) : a[i] * (cnt + 1) + b[i] * (cnt + 2)
+    	), cnt += 2;
+    	
+    	// fixing 1 index to 0 index
+    	fod(i,1,n) fup[i] -= suf[i], fown[i] -= suf[i];
+    	
+    	// cout << fup[1] << el;
+    	int res = -INF;
+    	fod(i,1,n){
+    		int ans = 0;
+    		
+    		if(i & 1) maxi(ans, zig[i-1] + fup[i] + suf[i] * 2 * (i - 1));
+    		else maxi(ans, zig[i-1] + fown[i] + suf[i] * 2 * (i- 1));
+    		
+    		maxi(res, ans);
+    	}
+    	
+    	cout << res;
+    }	
+}
+namespace sub2{
+	
+	void solve(){
+	
+	}
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0); srand(time(0)); 
+    if(fopen(name".inp", "r")){
+      freopen(name".inp", "r", stdin);
+      freopen(name".out", "w", stdout);
+    }
+    int Test = 1; if(multitest) cin >> Test;
+    init();
+    while(Test-- or endless){
+        inp();
+        sub1 :: solve();
+        sub2 :: solve();
+    }
+    kill();
+}
